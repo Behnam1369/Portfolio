ae959eba5c9027499d03c51a675a50511ccdcc9e
Behnam1369 <behnam.aghaali@yahoo.com>
2022-01-25 00:48:47 +0330

    Modify javascript line spacings

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-document.querySelector("i.fa").addEventListener("click",function(){
-    let menu = document.querySelector("#mobile-menu");
-    menu.style.display = "flex"
-    let closeButton = document.createElement("li")
-    closeButton.innerHTML = "<span class='closeMenu'></span>";
-    menu.insertBefore(closeButton, menu.firstChild);
-
-    closeButton.addEventListener("click",function(){
-        menu.style.display = "none"
-        closeButton.remove();
-    })
-
-    let items = document.querySelectorAll("#mobile-menu li")
-    console.log(items)
-    items.addEventListener("click",function(){
-        menu.style.display = "none"
-        closeButton.remove();
-        alert("it works")
-    })
-
-})
+document.querySelector("i.fa").addEventListener("click", function () {
+  let menu = document.querySelector("#mobile-menu");
+  menu.style.display = "flex";
+  let closeButton = document.createElement("li");
+  closeButton.innerHTML = "<span class='closeMenu'></span>";
+  menu.insertBefore(closeButton, menu.firstChild);
+  closeButton.addEventListener("click", function () {
+    menu.style.display = "none";
+    closeButton.remove();
+  });
+  let items = document.querySelectorAll("#mobile-menu li");
+  var itemsArr = Array.from(items);
+  itemsArr.map(function (el) {
+    el.addEventListener("click", function () {
+      menu.style.display = "none";
+      closeButton.remove();
+    });
+  });
+});
