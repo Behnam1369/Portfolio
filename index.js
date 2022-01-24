b11d71fdd6877d1e161c5529900dce68f239cb2b
Behnam1369 <behnam.aghaali@yahoo.com>
2022-01-25 01:13:45 +0330

    Modify linter errors

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-document.querySelector("i.fa").addEventListener("click", () => {
-  const menu = document.querySelector("#mobile-menu");
-  menu.style.display = "flex";
-  const closeButton = document.createElement("li");
-  const closeButtonSpan = document.createElement("span");
-  closeButtonSpan.classList.add("closeMenu");
+document.querySelector('i.fa').addEventListener('click', () => {
+  const menu = document.querySelector('#mobile-menu');
+  menu.style.display = 'flex';
+  const closeButton = document.createElement('li');
+  const closeButtonSpan = document.createElement('span');
+  closeButtonSpan.classList.add('closeMenu');
   closeButton.appendChild(closeButtonSpan);
   menu.insertBefore(closeButton, menu.firstChild);
-  closeButton.addEventListener("click", () => {
-    menu.style.display = "none";
+  closeButton.addEventListener('click', () => {
+    menu.style.display = 'none';
     closeButton.remove();
   });
-  const items = document.querySelectorAll("#mobile-menu li");
+  const items = document.querySelectorAll('#mobile-menu li');
   const itemsArr = Array.from(items);
   itemsArr.map((el) => {
-    return el.addEventListener("click", () => {
-      menu.style.display = "none";
+    return el.addEventListener('click', () => {
+      menu.style.display = 'none';
       closeButton.remove();
     });
   });
