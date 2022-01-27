b70421e2325bcaf29752de8393baf6ba0608de0a
Kakalanp <78335447+Kakalanp@users.noreply.github.com>
2022-01-27 13:21:44 -0500

    Added input for message and email

--- script.js
+++ script.js
@@ -259,12 +259,22 @@ submitButton.addEventListener('click', () => {
 var userInfo = { name: '', email: '', message: '' };
 if (localStorage.userInfo != undefined) {
   userInfo = JSON.parse(localStorage.userInfo);
-  nameBox.value = userInfo.name;
-  emailBox.value = userInfo.email;
-  messageBox.value = userInfo.message;
+  nameBox.value = (userInfo.name ? userInfo.name : '');
+  emailBox.value = (userInfo.email ? userInfo.email : '');
+  messageBox.value = (userInfo.message ? userInfo.message : '');
 }
 
 nameBox.addEventListener('input', () => {
   userInfo.name = nameBox.value;
+  localStorage.userInfo = JSON.stringify(userInfo);
+});
+
+emailBox.addEventListener('input', () => {
+  userInfo.email = emailBox.value;
+  localStorage.userInfo = JSON.stringify(userInfo);
+});
+
+messageBox.addEventListener('input', () => {
+  userInfo.message = messageBox.value;
   localStorage.userInfo = JSON.stringify(userInfo);
 });
