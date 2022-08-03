6e853cabb7d0bcf206ab2f375951e23d402ea107
behnam1369 <behnam.aghaali@yahoo.com>
2022-08-03 12:33:09 +0430

    update portfolio

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-document.querySelector('i.fa').addEventListener('click', () => {
-  const menu = document.querySelector('#mobile-menu');
-  menu.style.display = 'flex';
-  const closeButton = document.createElement('li');
-  const closeButtonSpan = document.createElement('span');
-  closeButtonSpan.classList.add('close-button');
+document.querySelector("i.fa").addEventListener("click", () => {
+  const menu = document.querySelector("#mobile-menu");
+  menu.style.display = "flex";
+  const closeButton = document.createElement("li");
+  const closeButtonSpan = document.createElement("span");
+  closeButtonSpan.classList.add("close-button");
   closeButton.appendChild(closeButtonSpan);
   menu.insertBefore(closeButton, menu.firstChild);
-  closeButton.addEventListener('click', () => {
-    menu.style.display = 'none';
+  closeButton.addEventListener("click", () => {
+    menu.style.display = "none";
     closeButton.remove();
   });
-  const items = document.querySelectorAll('#mobile-menu li');
+  const items = document.querySelectorAll("#mobile-menu li");
   const itemsArr = Array.from(items);
   for (let i = 0; i < itemsArr.length; i += 1) {
-    itemsArr[i].addEventListener('click', () => {
-      menu.style.display = 'none';
+    itemsArr[i].addEventListener("click", () => {
+      menu.style.display = "none";
       closeButton.remove();
     });
   }
@@ -22,72 +22,73 @@ document.querySelector('i.fa').addEventListener('click', () => {
 
 const projects = [
   {
-    img: 'images/work1.jpg',
-    title: 'Tour Hunter',
-    client: 'Microverse',
-    role: 'Full Stack Developer',
-    year: '2022',
-    tags: ['Ruby On Rials', 'React', 'Redux', 'Tailwind CSS', 'Postgres'],
-    liveLink: 'https://tourhunterapp.herokuapp.com/tours',
-    sourceLink: 'https://github.com/leopiresgoss/tour-hunter',
-    paragraph:
-      'TourHunter is a project made by a group of 5 developers to give users options of tours around the world to reserve and the admins to add and delete said tours. General users can create an account to be able to reserve a tour and only admins will be able to create and delete tours.',
+    img: "images/work0.jpg",
+    title: "Contract Management System",
+    client: "PECO",
+    role: "Full Stack Developer",
+    year: "20218-Now",
+    tags: ["ASP.Net", "javascript", "JQuery", "SQL Server"],
+    liveLink: "http://zlatan.pe-co.ir/",
+    sourceLink: "https://github.com/Behnam1369/PECO",
+    paragraph: `A contract management system for PECO. It allows the client to keep track of the contracts, invoices and the payments. It also contains a flexible workflow manager to design the process of reviewing invoices. And finally, users can get a variety range of reports based on contract, client, cost centers, currency, etc. <br />
+       When designing the workflow, Every review step can contain an stansard deadline to review the invoice. And the system will automatically send an email to the client when the deadline is reached. </br>
+       The client of this project is PECO (Pezhvak Energy Company) which is a well known and reputable drilling company.`,
   },
   {
-    img: 'images/work2.jpg',
-    title: 'Blog App',
-    client: 'Microverse',
-    role: 'Full Stack Developer',
-    year: '2022',
-    tags: ['Ruby', 'Rails', 'Postgres', 'Rspec'],
-    liveLink: 'https://rails-blog1000.herokuapp.com/',
-    sourceLink: 'https://github.com/Behnam1369/Portfolio',
+    img: "images/work1.jpg",
+    title: "Tour Hunter",
+    client: "Microverse",
+    role: "Full Stack Developer",
+    year: "2022",
+    tags: ["Ruby on Rials", "React", "Redux", "Tailwind CSS", "Postgres"],
+    liveLink: "https://tourhunterapp.herokuapp.com/tours",
+    sourceLink: "https://github.com/leopiresgoss/tour-hunter",
     paragraph:
-      'A simple blogging platform created using Ruby on Rails that alows users to create a profile, add Posts, like posts from other bloggers and add comment to posts. In this project I have used Devise gem for authentication, CanCanCan gem for authorization and Rspec/Capybara for integration tests. ',
+      "TourHunter is a project made by a group of 5 developers to give users options of tours around the world to reserve and the admins to add and delete said tours. General users can create an account to be able to reserve a tour and only admins will be able to create and delete tours.",
   },
   {
-    img: 'images/work3.jpg',
-    title: 'My Gym Mate',
-    client: 'Athletico',
-    role: 'Full Stack Developer',
-    year: '2018',
-    tags: ['html', 'css', 'javaScript', 'bootstrap'],
-    liveLink: 'https://behnam1369.github.io/Portfolio/index.html',
-    sourceLink: 'https://github.com/Behnam1369/Portfolio',
+    img: "images/work2.jpg",
+    title: "Blog App",
+    client: "Microverse",
+    role: "Full Stack Developer",
+    year: "2022",
+    tags: ["Ruby on Rails", "Postgres", "Rspec"],
+    liveLink: "https://rails-blog1000.herokuapp.com/",
+    sourceLink: "https://github.com/Behnam1369/Portfolio",
     paragraph:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+      "A simple blogging platform created using Ruby on Rails that alows users to create a profile, add Posts, like posts from other bloggers and add comment to posts. In this project I have used Devise gem for authentication, CanCanCan gem for authorization and Rspec/Capybara for integration tests. ",
   },
   {
-    img: 'images/work4.jpg',
-    title: 'My Gym Mate',
-    client: 'Athletico',
-    role: 'Full Stack Developer',
-    year: '2018',
-    tags: ['html', 'css', 'javaScript', 'github', 'ruby'],
-    liveLink: 'https://behnam1369.github.io/Portfolio/index.html',
-    sourceLink: 'https://github.com/Behnam1369/Portfolio',
+    img: "images/work3.jpg",
+    title: "Budget App",
+    client: "Microverse",
+    role: "Full Stack Developer",
+    year: "2022",
+    tags: ["Ruby on Rails", "Postgres", "Rspec"],
+    liveLink: "https://myexpenditures.herokuapp.com/",
+    sourceLink: "https://github.com/Behnam1369/budget-app",
     paragraph:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+      "Budget app is a mobile application that helps users to keep track of their expenditures. It allows users to add expenditure categories and add expenditures to any category. Finally they can see the total spent amount for each category. ",
   },
   {
-    img: 'images/work5.jpg',
-    title: 'To-do List',
-    client: 'Microverse',
-    role: 'Full Stack Developer',
-    year: '2022',
-    tags: ['html', 'css', 'javaScript', 'webpack'],
-    liveLink: 'https://behnam1369.github.io/to-do-list/',
-    sourceLink: 'https://github.com/Behnam1369/to-do-list',
+    img: "images/work5.jpg",
+    title: "To-do List",
+    client: "Microverse",
+    role: "Full Stack Developer",
+    year: "2022",
+    tags: ["html", "css", "javaScript", "webpack"],
+    liveLink: "https://behnam1369.github.io/to-do-list/",
+    sourceLink: "https://github.com/Behnam1369/to-do-list",
     paragraph:
-      'This project is focused on using webpack for creating an optimized deploy. In this project you can add a task, edit a task, mark a task as completed, remove a task or remove all completed tasks. After any interaction all tasks will be automatically saved into the local storage and They will be recalled after reopnening the page.   ',
+      "This project is focused on using webpack for creating an optimized deploy. In this project you can add a task, edit a task, mark a task as completed, remove a task or remove all completed tasks. After any interaction all tasks will be automatically saved into the local storage and They will be recalled after reopnening the page.   ",
   },
 ];
 
 function createWorks() {
-  const works = document.querySelector('#works');
+  const works = document.querySelector("#works");
   projects.map((el, i) => {
-    const work = document.createElement('div');
-    work.classList.add('work');
+    const work = document.createElement("div");
+    work.classList.add("work");
     work.innerHTML = `<img src='${el.img}' alt='Work ${i}' />
         <div>
           <h3>${el.title}</h3>
@@ -98,7 +99,7 @@ function createWorks() {
           <span class='span2'>${el.year}</span>
           <p>${el.paragraph}</p>
           <ul class='tags'>
-          ${el.tags.map((t) => `<li class='tag'>${t}</li>`).join('')}    </ul>
+          ${el.tags.map((t) => `<li class='tag'>${t}</li>`).join("")}    </ul>
           <div class='break'></div>
           <button>See Project</button>
         </div>
@@ -109,124 +110,127 @@ function createWorks() {
 }
 
 function craetePopups() {
-  const button = document.querySelectorAll('.work button');
+  const button = document.querySelectorAll(".work button");
   const buttonArr = Array.from(button);
   buttonArr.map((el, i) => {
-    el.addEventListener('click', () => {
-      const popupContainer = document.createElement('div');
-      popupContainer.classList.add('popup-container');
+    el.addEventListener("click", () => {
+      const popupContainer = document.createElement("div");
+      popupContainer.classList.add("popup-container");
 
-      const popup = document.createElement('div');
-      popup.classList.add('popup');
+      const popup = document.createElement("div");
+      popup.classList.add("popup");
 
       popupContainer.appendChild(popup);
 
-      const closeButton = document.createElement('span');
-      closeButton.classList.add('close-button');
-      closeButton.addEventListener('click', () => {
+      const closeButton = document.createElement("span");
+      closeButton.classList.add("close-button");
+      closeButton.addEventListener("click", () => {
         popupContainer.remove();
-        document.querySelector('html').style.overflow = 'auto';
+        document.querySelector("html").style.overflow = "auto";
       });
       popup.appendChild(closeButton);
 
-      const title = document.createElement('h2');
+      const title = document.createElement("h2");
       title.innerHTML = projects[i].title;
       popup.appendChild(title);
 
-      const client = document.createElement('span');
+      const client = document.createElement("span");
       client.innerHTML = projects[i].client;
-      client.classList.add('span1');
+      client.classList.add("span1");
       popup.appendChild(client);
 
-      const devider1 = document.createElement('i');
-      devider1.classList.add('devider');
+      const devider1 = document.createElement("i");
+      devider1.classList.add("devider");
       popup.appendChild(devider1);
 
-      const role = document.createElement('span');
+      const role = document.createElement("span");
       role.innerHTML = projects[i].role;
-      role.classList.add('span2');
+      role.classList.add("span2");
       popup.appendChild(role);
 
-      const devider2 = document.createElement('i');
-      devider2.classList.add('devider');
+      const devider2 = document.createElement("i");
+      devider2.classList.add("devider");
       popup.appendChild(devider2);
 
-      const year = document.createElement('span');
+      const year = document.createElement("span");
       year.innerHTML = projects[i].year;
-      year.classList.add('span2');
+      year.classList.add("span2");
       popup.appendChild(year);
 
-      const img = document.createElement('img');
-      img.setAttribute('src', projects[i].img);
+      const img = document.createElement("img");
+      img.setAttribute("src", projects[i].img);
       popup.appendChild(img);
 
-      const paragraph = document.createElement('p');
+      const paragraph = document.createElement("p");
       paragraph.innerHTML = projects[i].paragraph;
       popup.appendChild(paragraph);
 
-      const tagsAndButtonsDiv = document.createElement('div');
-      tagsAndButtonsDiv.classList.add('tags-buttons');
+      const tagsAndButtonsDiv = document.createElement("div");
+      tagsAndButtonsDiv.classList.add("tags-buttons");
 
-      const tags = document.createElement('ul');
+      const tags = document.createElement("ul");
       projects[i].tags.map((t) => {
-        const tag = document.createElement('li');
-        tag.classList.add('tag');
+        const tag = document.createElement("li");
+        tag.classList.add("tag");
         tag.innerHTML = t;
         tags.appendChild(tag);
         return null;
       });
       tagsAndButtonsDiv.appendChild(tags);
 
-      const seeLiveButton = document.createElement('a');
-      seeLiveButton.setAttribute('href', projects[i].liveLink);
-      seeLiveButton.classList.add('see-project-button');
-      seeLiveButton.innerHTML = '<button> See Live </button>';
+      const seeLiveButton = document.createElement("a");
+      seeLiveButton.setAttribute("href", projects[i].liveLink);
+      seeLiveButton.classList.add("see-project-button");
+      seeLiveButton.innerHTML = "<button> See Live </button>";
       tagsAndButtonsDiv.appendChild(seeLiveButton);
 
-      const seeSourceButton = document.createElement('a');
-      seeSourceButton.setAttribute('href', projects[i].sourceLink);
-      seeSourceButton.classList.add('see-source-button');
-      seeSourceButton.innerHTML = '<button> See Source </button>';
+      const seeSourceButton = document.createElement("a");
+      seeSourceButton.setAttribute("href", projects[i].sourceLink);
+      seeSourceButton.classList.add("see-source-button");
+      seeSourceButton.innerHTML = "<button> See Source </button>";
       tagsAndButtonsDiv.appendChild(seeSourceButton);
 
       popup.appendChild(tagsAndButtonsDiv);
 
-      document.querySelector('html').style.overflow = 'hidden';
-      document.querySelector('body').appendChild(popupContainer);
+      document.querySelector("html").style.overflow = "hidden";
+      document.querySelector("body").appendChild(popupContainer);
     });
     return null;
   });
 }
 
 document.addEventListener(
-  'DOMContentLoaded',
+  "DOMContentLoaded",
   () => {
     createWorks();
     craetePopups();
   },
-  false,
+  false
 );
 
-const form = document.getElementsByTagName('form')[0];
-const emailBox = document.getElementById('email');
-const nameBox = document.getElementById('name');
-const messageBox = document.getElementById('message');
-const error = document.getElementsByClassName('error-message')[0];
-const submitButton = document.querySelector('.submit-button');
+const form = document.getElementsByTagName("form")[0];
+const emailBox = document.getElementById("email");
+const nameBox = document.getElementById("name");
+const messageBox = document.getElementById("message");
+const error = document.getElementsByClassName("error-message")[0];
+const submitButton = document.querySelector(".submit-button");
 
 function showError(text) {
   // if (!error.style.opacity) {
-  emailBox.addEventListener('input', () => {
-    if (emailBox.validity.valid && emailBox.value.toLowerCase() === emailBox.value) {
-      emailBox.style.boxShadow = '';
+  emailBox.addEventListener("input", () => {
+    if (
+      emailBox.validity.valid &&
+      emailBox.value.toLowerCase() === emailBox.value
+    ) {
+      emailBox.style.boxShadow = "";
       error.style.opacity = 0;
     } else {
       error.style.opacity = 1;
-      emailBox.style.boxShadow = 'red 0 3px 0 0, red 0 0 0 3px';
+      emailBox.style.boxShadow = "red 0 3px 0 0, red 0 0 0 3px";
     }
   });
   error.style.opacity = 1;
-  emailBox.style.boxShadow = 'red 0 3px 0 0, red 0 0 0 3px';
+  emailBox.style.boxShadow = "red 0 3px 0 0, red 0 0 0 3px";
   // }
 
   const outInterval = setInterval(() => {
@@ -244,37 +248,37 @@ function showError(text) {
   }, 200 / 50);
 }
 
-submitButton.addEventListener('click', () => {
+submitButton.addEventListener("click", () => {
   if (emailBox.value.toLowerCase() !== emailBox.value) {
-    showError('All email letters should be in lower case.');
+    showError("All email letters should be in lower case.");
   } else if (emailBox.validity.valueMissing) {
-    showError('Please enter your email address.');
+    showError("Please enter your email address.");
   } else if (emailBox.validity.typeMismatch) {
-    showError('Please enter a valid email address.');
+    showError("Please enter a valid email address.");
   } else {
     form.submit();
   }
 });
 
-let userInfo = { name: '', email: '', message: '' };
+let userInfo = { name: "", email: "", message: "" };
 if (localStorage.userInfo !== undefined) {
   userInfo = JSON.parse(localStorage.userInfo);
-  nameBox.value = (userInfo.name ? userInfo.name : '');
-  emailBox.value = (userInfo.email ? userInfo.email : '');
-  messageBox.value = (userInfo.message ? userInfo.message : '');
+  nameBox.value = userInfo.name ? userInfo.name : "";
+  emailBox.value = userInfo.email ? userInfo.email : "";
+  messageBox.value = userInfo.message ? userInfo.message : "";
 }
 
-nameBox.addEventListener('input', () => {
+nameBox.addEventListener("input", () => {
   userInfo.name = nameBox.value;
   localStorage.userInfo = JSON.stringify(userInfo);
 });
 
-emailBox.addEventListener('input', () => {
+emailBox.addEventListener("input", () => {
   userInfo.email = emailBox.value;
   localStorage.userInfo = JSON.stringify(userInfo);
 });
 
-messageBox.addEventListener('input', () => {
+messageBox.addEventListener("input", () => {
   userInfo.message = messageBox.value;
   localStorage.userInfo = JSON.stringify(userInfo);
 });
