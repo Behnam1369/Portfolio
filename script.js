497ebd8efc5c72fd50dd555d64f91d8ae59af2ef
behnam1369 <behnam.aghaali@yahoo.com>
2023-02-25 17:12:30 +0330

    Apply image gallery instead of a single image

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ document.querySelector('i.fa-bars').addEventListener('click', () => {
 
 const projects = [
   {
-    img: 'images/work1.jpg',
+    media: [{ type: 'image', src: 'images/work1.jpg' },{ type: 'image', src: 'images/work2.jpg' }],
     title: 'Tour Hunter',
     client: 'Microverse',
     role: 'Full Stack Developer',
@@ -34,7 +34,7 @@ const projects = [
       'TourHunter is a project made by a group of 5 developers to give users options of tours around the world to reserve and the admins to add and delete said tours. General users can create an account to be able to reserve a tour and only admins will be able to create and delete tours.',
   },
   {
-    img: 'images/work2.jpg',
+    media: [{ type: 'image', src: 'images/work2.jpg' }],
     title: 'Blog App',
     client: 'Microverse',
     role: 'Full Stack Developer',
@@ -46,7 +46,7 @@ const projects = [
       'A simple blogging platform created using Ruby on Rails that alows users to create a profile, add Posts, like posts from other bloggers and add comment to posts. In this project I have used Devise gem for authentication, CanCanCan gem for authorization and Rspec/Capybara for integration tests. ',
   },
   {
-    img: 'images/work3.jpg',
+    media: [{ type: 'image', src: 'images/work3.jpg' }],
     title: 'Budget App',
     client: 'Microverse',
     role: 'Full Stack Developer',
@@ -58,7 +58,7 @@ const projects = [
       'Budget app is a mobile application that helps users to keep track of their expenditures. It allows users to add expenditure categories and add expenditures to any category. Finally they can see the total spent amount for each category. ',
   },
   {
-    img: 'images/work5.jpg',
+    media: [{ type: 'image', src: 'images/work5.jpg' }],
     title: 'To-do List',
     client: 'Microverse',
     role: 'Full Stack Developer',
@@ -70,7 +70,7 @@ const projects = [
       'This project is focused on using webpack for creating an optimized deploy. In this project you can add a task, edit a task, mark a task as completed, remove a task or remove all completed tasks. After any interaction all tasks will be automatically saved into the local storage and They will be recalled after reopnening the page.   ',
   },
   {
-    img: 'images/work0.jpg',
+    media: [{ type: 'image', src: 'images/work0.jpg' }],
     title: 'Contract Management System',
     client: 'PECO',
     role: 'Full Stack Developer',
@@ -86,10 +86,18 @@ const projects = [
 
 function createWorks() {
   const works = document.querySelector('#works');
-  projects.map((el, i) => {
+  projects.map((el) => {
     const work = document.createElement('div');
     work.classList.add('work');
-    work.innerHTML = `<img src='${el.img}' alt='Work ${i}' />
+    work.innerHTML = `
+        <div>
+  ${el.media.map((med, i) => {
+    if (med.type === 'image') {
+      return `<img src='${med.src}' alt='${el.title}' class='${i === 0 ? 'visible' : ''}' />`;
+    }
+    return '';
+  }).join('')}
+        </div>
         <div>
           <h3>${el.title}</h3>
           <span class='span1'>${el.client}</span>
@@ -101,16 +109,49 @@ function createWorks() {
           <ul class='tags'>
           ${el.tags.map((t) => `<li class='tag'>${t}</li>`).join('')}    </ul>
           <div class='break'></div>
-          <button>See Project</button>
+          <button class="btnProject">See Project</button>
         </div>
     `;
     works.appendChild(work);
+
+    //Next Button
+    const btnNext = document.createElement('button');
+    btnNext.classList.add('next');
+    btnNext.innerHTML = '<i class="fas fa-chevron-right"></i>';
+    btnNext.addEventListener('click', () => {
+      const img = work.querySelector('img.visible');
+      if (img.nextElementSibling.tagName === 'IMG') {
+        img.classList.remove('visible');
+        img.nextElementSibling.classList.add('visible');
+      } else {
+        img.classList.remove('visible');
+        img.parentElement.children[1].classList.add('visible');
+      }
+    });
+    work.children[0].appendChild(btnNext);
+
+    // Previous Button
+    const btnPrev = document.createElement('button');
+    btnPrev.classList.add('prev');
+    btnPrev.innerHTML = '<i class="fas fa-chevron-left"></i>';
+    btnPrev.addEventListener('click', () => {
+      const img = work.querySelector('img.visible');
+      if (img.previousElementSibling.tagName === 'IMG') {
+        img.classList.remove('visible');
+        img.previousElementSibling.classList.add('visible');
+      } else {
+        img.classList.remove('visible');
+        img.parentElement.children[img.parentElement.children.length - 2].classList.add('visible');
+      }
+    });
+    work.children[0].prepend(btnPrev);
+
     return null;
   });
 }
 
 function craetePopups() {
-  const button = document.querySelectorAll('.work button');
+  const button = document.querySelectorAll('.work button.btnProject');
   const buttonArr = Array.from(button);
   buttonArr.map((el, i) => {
     el.addEventListener('click', () => {
